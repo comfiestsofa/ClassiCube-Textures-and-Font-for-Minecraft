04863748839442508bb91f4ccccab8dede66c9bf
sofa <sophiapark0@proton.me>
2026-02-28 12:41:13 -0800

    update bedrock script, not really done yet

--- update-bedrock.sh
+++ update-bedrock.sh
@@ -1,10 +1,24 @@
 #!/usr/bin/env bash
-rm -rf "bedrock/"*"/"*"/" # delete all folders inside each bedrock pack
+
+# delete all folders inside each bedrock pack
+rm -rf "bedrock/"*"/"*"/"
+
 for pack in ClassiCube*/; do
 	packname="$(basename "$pack")"
+
+	# copy pack.png to pack_icon.png
 	cp -a "$pack/pack.png" "bedrock/$packname/pack_icon.png"
+
+	cp -a "$pack/_CREDITS.txt" "bedrock/$packname/"
+
+	# copy content folders
 	cp -a "$pack/assets/minecraft/"* "bedrock/$packname/"
+
+	# bedrock renamed block to blocks
 	mv "bedrock/$packname/textures/block" "bedrock/$packname/textures/blocks"
+
+	# bedrock doesn't support changing vanilla block models
+	rm -rf "bedrock/$packname/models"
 done
 
 exit
