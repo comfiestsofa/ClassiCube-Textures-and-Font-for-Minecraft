1f36210d9161029accce5e9d0bd7434bde8faf0b
sofa <sophiapark0@proton.me>
2026-02-28 11:20:24 -0800

    update zipper

--- zip.sh
+++ zip.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/env bash
-rm -rf build
-mkdir build
+version="1.0"
 
 for pack in "ClassiCube"*; do
 	# command line zip is weird and will include the enclosing folder unless you run cd
 	cd "$pack"
-	zip -x "*Thumbs.db" -x "*DS_Store*" -9r "../build/$pack.zip" *
+	zip -x "*Thumbs.db" -x "*DS_Store*" -9r "../build/$pack $version.zip" *
 	cd ..
 done
 
 cd bedrock
 for pack in "ClassiCube"*; do
 	cd "$pack"
-	zip -x "*Thumbs.db" -x "*DS_Store*" -9r "../../build/$pack.mcpack" *
+	zip -x "*Thumbs.db" -x "*DS_Store*" -9r "../../build/$pack $version.mcpack" *
 	cd ..
 done
 
