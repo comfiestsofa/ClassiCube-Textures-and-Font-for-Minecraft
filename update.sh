a7b8345d36477838cd5a36e0d2a9f8752215d8cf
sofa <sophiapark0@proton.me>
2026-02-27 21:47:05 -0800

    aio script

---
+++ update.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+rm -rf "ClassiCube All-In-One/assets"
+for pack in \
+	"ClassiCube Font" \
+	"ClassiCube Main" \
+	"ClassiCube Mobs (MC)" \
+	"ClassiCube UI"; do
+		cp -a "$pack/assets" "ClassiCube All-In-One/"
+done
+
+exit
