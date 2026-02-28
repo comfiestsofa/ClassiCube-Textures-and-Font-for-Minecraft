e2051a11b5ed96f2fd0985985d3ce95573e195db
sofa <sophiapark0@proton.me>
2026-02-28 09:47:18 -0800

    fix script

--- png-smol.sh
+++ png-smol.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-exiftool -all= -overwrite_original "${[@]}"
-oxipng -o max -p -vvvvvvvv --fix -Z "${[@]}"
+exiftool -all= -overwrite_original "$@"
+oxipng -o max -p -vvvvvvvv --fix -Z "$@"
