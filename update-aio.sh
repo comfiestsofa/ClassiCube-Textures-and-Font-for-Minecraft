44ea8806ee03d5272d897b6aa5f086d8c2e5e11d
sofa <sophiapark0@proton.me>
2026-02-28 14:02:08 -0800

    changed the name

--- update-aio.sh
+++ update-aio.sh
@@ -2,7 +2,7 @@
 rm -rf "ClassiCube All-In-One/assets"
 for pack in \
 	"ClassiCube Font" \
-	"ClassiCube Main" \
+	"ClassiCube Blocks, FX" \
 	"ClassiCube Mobs (MC)" \
 	"ClassiCube UI"; do
 		cp -a "$pack/assets" "ClassiCube All-In-One/"
